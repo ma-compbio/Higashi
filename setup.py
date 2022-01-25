4b6ae7af029989430123bade885599b7af60e543
Ruochi Zhang <ruochiz@andrew.cmu.edu>
2022-01-25 17:53:47 -0500

    add new parameter route_nn to direct the bin nodes to the corresponding NN without using mapping table.

--- setup.py
+++ setup.py
@@ -7,22 +7,6 @@ setup(
     url='https://github.com/ma-compbio/Higashi',
     include_package_data=True,
     packages=find_packages(exclude=['Temp', 'config_dir', 'figs']),
-    install_requires=[
-        'python>=3.7.0',
-        'h5py',
-        'numpy',
-        'pandas',
-        'pytorch>=1.7.0',
-        'fbpca',
-        'scikit-learn>=0.23.2',
-        'tqdm',
-        'cooler>=0.8',
-        'seaborn',
-        'matplotlib',
-        'umap-learn',
-        'bokeh>=2.1.1',
-        'Pillow'
-    ],
     author='Ruochi Zhang',
     author_email='ruochiz@andrew.cmu.edu',
     license='MIT'
